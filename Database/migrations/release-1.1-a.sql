cdfba0522136d2e0c6ad9f29ecf442be0252ff99
sammyreles22 <samuel.reles@gmail.com>
2025-11-02 23:10:48 -0500

    testing

---
+++ Database/migrations/release-1.1-a.sql
@@ -0,0 +1,4 @@
+/**
+ * Event to remove data from COLLECTED_DATA table after 7 days.
+ */
+CREATE EVENT remove_collected_data ON SCHEDULE EVERY 1 DAY DO DELETE FROM COLLECTED_DATA WHERE pull_date < NOW() - INTERVAL 7 DAY;
